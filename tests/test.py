243fd42c345bdcb2c7a87389cc4a4e857cb12852
Liyang-Zhang <1219187848@qq.com>
2023-12-22 13:13:38 +0800

    Refactor: put raw script into a function and add TODO functions

--- tests/test.py
+++ tests/test.py
@@ -1,72 +1,77 @@
 import json
+from pathlib import Path
 
 import pandas as pd
-
-config_path = (
-    "/home/leon/repo/pharmGKB-extraction/pharmgkb_extraction/config/custom_class.json"
-)
-
-with open(config_path, "r") as config_file:
-    data = json.load(config_file)
-
-gene_list = [gene.lower() for gene in data["gene"]]
-drug_list = [drug.lower() for drug in data["drug"]]
-phenotype_list_ld = [drug.lower() for drug in data["phenotype_ld"]]
-phenotype_list = [phenotype.lower() for phenotype in data["phenotype"]]
-level_list = data["level"]
-
-print(gene_list)
-print(drug_list)
-print(phenotype_list)
-print(level_list)
-
-clinical_annotations_path = "/home/leon/data/lung65/chemo/clinical_annotations.tsv"
-
-df_cli_anno = pd.read_csv(clinical_annotations_path, sep="\t", header=0)
-
-# Apply all conditions in a single line
-# filtered_df = df_cli_anno[
-#    df_cli_anno["Drug(s)"].apply(
-#        lambda x: isinstance(x, str) and any(drug in x.lower() for drug in drug_list)
-#    )
-#    & df_cli_anno["Gene"].apply(
-#        lambda x: isinstance(x, str) and any(gene in x.lower() for gene in gene_list)
-#    )
-#    &
-#    df_cli_anno["Phenotype(s)"].apply(
-#        lambda x: isinstance(x, str)
-#        and any(phenotype in x for phenotype in phenotype_list)
-#    )
-#    & df_cli_anno["Level of Evidence"].apply(
-#        lambda x: isinstance(x, str) and any(level in x for level in level_list)
-#    )
-# ]
 
 
 def any_in_string(items, string):
     return any(item in string.lower() for item in items)
 
 
-df_cli_anno["phenotype_ld_1219"] = df_cli_anno["Phenotype(s)"].apply(
-    lambda x: any_in_string(phenotype_list_ld, x) if isinstance(x, str) else False
-)
-df_cli_anno["Matches Drug"] = df_cli_anno["Drug(s)"].apply(
-    lambda x: any_in_string(drug_list, x) if isinstance(x, str) else False
-)
-df_cli_anno["Matches Gene"] = df_cli_anno["Gene"].apply(
-    lambda x: any_in_string(gene_list, x) if isinstance(x, str) else False
-)
-df_cli_anno["Matches Phenotype"] = df_cli_anno["Phenotype(s)"].apply(
-    lambda x: any_in_string(phenotype_list, x) if isinstance(x, str) else False
-)
-df_cli_anno["Matches Level"] = df_cli_anno["Level of Evidence"].apply(
-    lambda x: x in level_list if isinstance(x, str) else False
-)
+def add_common_filter_columns(
+    df_cli_anno: pd.DataFrame, config_path: Path
+) -> pd.DataFrame:
+    with open(str(config_path), "r") as config_file:
+        data = json.load(config_file)
+
+    gene_list = [gene.lower() for gene in data["gene"]]
+    drug_list = [drug.lower() for drug in data["drug"]]
+    phenotype_list_ld = [drug.lower() for drug in data["phenotype_ld"]]
+    phenotype_list = [phenotype.lower() for phenotype in data["phenotype"]]
+    level_list = data["level"]
+
+    print(gene_list)
+    print(drug_list)
+    print(phenotype_list)
+    print(level_list)
+
+    df_cli_anno["phenotype_ld_1219"] = df_cli_anno["Phenotype(s)"].apply(
+        lambda x: any_in_string(phenotype_list_ld, x) if isinstance(x, str) else False
+    )
+    df_cli_anno["Matches Drug"] = df_cli_anno["Drug(s)"].apply(
+        lambda x: any_in_string(drug_list, x) if isinstance(x, str) else False
+    )
+    df_cli_anno["Matches Gene"] = df_cli_anno["Gene"].apply(
+        lambda x: any_in_string(gene_list, x) if isinstance(x, str) else False
+    )
+    df_cli_anno["Matches Phenotype"] = df_cli_anno["Phenotype(s)"].apply(
+        lambda x: any_in_string(phenotype_list, x) if isinstance(x, str) else False
+    )
+    df_cli_anno["Matches Level"] = df_cli_anno["Level of Evidence"].apply(
+        lambda x: x in level_list if isinstance(x, str) else False
+    )
+
+    return df_cli_anno
 
 
-# Output the filtered DataFrame to a new TSV file
-output_path = "/home/leon/repo/pharmGKB-extraction/tests/lung65_clinical_annotation.tsv"
+# TODO Add genome location to each row
+# Input: 1. matched result VCF file or bed file
+def add_hg19_genome_location_info():
+    ...
 
-df_cli_anno.to_csv(output_path, sep="\t", index=False)
 
-print(f"Filtered data has been saved to {output_path}")
+# TODO Add columns to tell if the record rs ID is in the designed probe intervals
+# Input: 1. porbe ned files
+def add_if_covered_by_probe_info():
+    ...
+
+
+def main():
+    config_path = Path(
+        "/Users/zhangliyang/repo/pharmGKB-extraction/pharmgkb_extraction/config/custom_class.json"
+    )
+    clinical_annotations_path = Path(
+        "/Users/zhangliyang/repo/pharmGKB-extraction/tests/clinicalAnnotations/clinical_annotations.tsv"
+    )
+
+    df_cli_anno = pd.read_csv(clinical_annotations_path, sep="\t", header=0)
+    df_cli_anno = add_common_filter_columns(df_cli_anno, config_path)
+
+    output_path = "/Users/zhangliyang/repo/pharmGKB-extraction/tests/clinicalAnnotations/lung65_clinical_annotation.tsv"
+    df_cli_anno.to_csv(output_path, sep="\t", index=False)
+
+    print(f"Filtered data has been saved to {output_path}")
+
+
+if __name__ == "__main__":
+    main()
