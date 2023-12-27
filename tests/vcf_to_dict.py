2d3d9872f5eecfaea5744c827bd1689405de2ce5
Liyang-Zhang <1219187848@qq.com>
2023-12-27 11:52:04 +0800

    Style: Change file structure

--- tests/vcf_to_dict.py
+++ tests/vcf_to_dict.py
@@ -4,7 +4,10 @@ from pathlib import Path
 
 
 # Convert vcf to dicts by stream processing
-def process_vcf_stream(gzipped_vcf_file_path, gzipped_json_file_path):
+def process_vcf_stream(gzipped_vcf_file_path: Path, gzipped_json_file_path: Path):
+    """
+    Converts VCF file to JSON format.
+    """
     with gzip.open(gzipped_vcf_file_path, "rt") as vcf_file, gzip.open(
         gzipped_json_file_path, "wt"
     ) as output_file:
@@ -20,7 +23,12 @@ def process_vcf_stream(gzipped_vcf_file_path, gzipped_json_file_path):
 
 
 # Convert all dicts into single dict with multiple json file
-def batch_process_json_objects(gzipped_json_file_path, batch_size, output_file_path):
+def batch_process_json_objects(
+    gzipped_json_file_path: Path, batch_size: int, output_file_path: Path
+):
+    """
+    Processes JSON objects in batches.
+    """
     batch_count = 0
     current_batch = {}
 
@@ -42,13 +50,16 @@ def batch_process_json_objects(gzipped_json_file_path, batch_size, output_file_p
             )
 
 
-def write_batch_to_file(batch_data, file_name):
+def write_batch_to_file(batch_data: dict, file_name: str):
     with gzip.open(file_name, "wt") as file:
         json.dump(batch_data, file)
 
 
 # Extract vcf record in a given rs list in all sub json files
 def process_all_sub_json_files(json_dir: Path, rs_list: Path, output_vcf: Path):
+    """
+    Processes sub JSON files to extract specific VCF records.
+    """
     for json_file in json_dir.glob(
         "*.json.gz"
     ):  # Iterate over all .json.gz files in the directory
@@ -67,22 +78,25 @@ def process_all_sub_json_files(json_dir: Path, rs_list: Path, output_vcf: Path):
                     out_f.write(rs_record + "\n")
 
 
-gzipped_vcf_file_path = (
+gzipped_vcf_file_path = Path(
     "/home/leon/data/lung65/dbSNP_latest_release/GCF_000001405.25.gz"
 )
-gzipped_json_file_path = (
+
+gzipped_json_file_path = Path(
     "/home/leon/data/lung65/dbSNP_latest_release/GCF_000001405.25.json.gz"
 )
-# merged_dict_file_path = "/home/leon/data/lung65/dbSNP_latest_release/GCF_000001405.25.merged.json.gz"
+
+batch_dir = Path("/home/leon/data/lung65/dbSNP_latest_release/GCF_000001405.25.merged")
+
 rs_list = Path(
     "/home/leon/data/lung65/dbSNP_latest_release/uniq_VariantHaplotypes_list.txt"
 )
-json_file = Path(
-    "/home/leon/data/lung65/dbSNP_latest_release/GCF_000001405.25.merged_0.json.gz"
-)
+
 json_dir = Path("/home/leon/data/lung65/dbSNP_latest_release/dbSNP156_sub_jsons")
+
 output_vcf = Path("/home/leon/data/lung65/dbSNP_latest_release/test.out.vcf")
 
-# process_vcf_stream(gzipped_vcf_file_path, gzipped_json_file_path)
-# batch_process_json_objects(gzipped_json_file_path, 10000000, "/home/leon/data/lung65/dbSNP_latest_release/GCF_000001405.25.merged")
+# Time consuming, should run one function a time
+process_vcf_stream(gzipped_vcf_file_path, gzipped_json_file_path)
+batch_process_json_objects(gzipped_json_file_path, 10000000, batch_dir)
 process_all_sub_json_files(json_dir, rs_list, output_vcf)
